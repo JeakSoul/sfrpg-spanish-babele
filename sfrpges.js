15756c023415e5d77f0cbd68cc91e6a93186b971
JeakSoul <30824015+JeakSoul@users.noreply.github.com>
2022-08-22 14:36:10 +0200

    Add files via upload

---
+++ sfrpges.js
@@ -0,0 +1,16 @@
+/* globals Hooks, game, Babele,console */
+
+Hooks.on('init', () => {
+    console.log ("INITIALIZING SPANISH TRANSLATION BABELE INIT");
+    if(typeof Babele !== 'undefined') {
+        Babele.get().register({
+            module: 'sfrpg-spanish-babele',
+            lang: 'es',
+            dir: 'compendiums/es'
+        });
+    }
+});
+
+Hooks.once("babele.ready", () => {
+    game.sfrpg.ConditionManager.initialize();
+});
